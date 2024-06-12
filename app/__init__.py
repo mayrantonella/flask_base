09ebf40584f1d6a3df8b2da59259fd313ff63b46
Mayra <a_mrebolledo@gmail.org>
2024-06-12 14:13:54 -0400

    mi primer commit

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+"""Configuración de la aplicación"""
+
+# Flask
+from flask import Flask
+
+app = Flask(__name__)
---
+++ server.py
@@ -0,0 +1,7 @@
+"""Server app."""
+
+from app import app
+
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    app.run(debug=True)
